6cb9e07cbfa1b035be8e71cc21453151505504f5
Victor-Monnier <122950171+Victor-Monnier@users.noreply.github.com>
2023-03-09 16:50:41 -0500

    Delete main directory

--- main/CollisionChecker.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import javafx.scene.shape.Rectangle;
-
-public class CollisionChecker {
-    public static boolean checkCollision (Rectangle a, Rectangle b) {
-        return a.intersects(b.getBoundsInLocal());
-    }
-}
