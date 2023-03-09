aeced9f25827070acf276420feefa00319b26bc1
Victor-Monnier <122950171+Victor-Monnier@users.noreply.github.com>
2023-03-09 16:48:19 -0500

    Add files via upload

--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -3,6 +3,7 @@ package main;
 import javafx.scene.shape.Rectangle;
 
 public class CollisionChecker {
+    //Returms true if both rectangles intersect
     public static boolean checkCollision (Rectangle a, Rectangle b) {
         return a.intersects(b.getBoundsInLocal());
     }
